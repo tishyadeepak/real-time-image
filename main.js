54ed196e9179d78007c8d03d8771613ae335abb4
tishyadeepak <91376226+tishyadeepak@users.noreply.github.com>
2022-10-05 18:09:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var pervious_result = '';
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaed);
+
+}
+
+function draw() {
+
+  image(video, 0, 0, 300.300);
+  classifier.classify(video, gotResult);
+
+}
+
+function modelLoaed() {
+
+  console.log('MobilNet model is loaed');
+
+} 
+
+function gotResult(error,results) {
+if(error){
+  console.error(error);
+}
+else{
+  if((results[0].confidence > 0.5) &&( pervious_result != result[0].label) ) {
+    console.log(results);
+   pervious_result= result[0].label;
+   var synth = window.speechSynthesis;
+   speak_data = 'Object detected is - '+results[0].label;
+   var utterThis = new SpeechSynthesisUtterance(speak_data);
+   synth.speak(utterThis);
+   document.getElementById("result_object_name").innerHTML = results[0].label;
+   document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+  }
+  
+}
+
+
+}
+
+
+
